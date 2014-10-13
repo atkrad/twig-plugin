214e932762cea48d6141e6a390bbdc3cbc88bc8a
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2014-10-13 14:06:06 +0330

    Fix bug in custom view file name

--- src/View/TwigView.php
+++ src/View/TwigView.php
@@ -8,7 +8,9 @@ use Cake\Event\Event;
 use Cake\Event\EventManager;
 use Cake\Network\Request;
 use Cake\Network\Response;
+use Cake\Utility\Hash;
 use Cake\Utility\Inflector;
+use Cake\View\Exception\MissingViewException;
 use Cake\View\View;
 use Twig\Extension\Twig_Extension_Basic;
 use Twig\Extension\Twig_Extension_I18n;
@@ -40,6 +42,15 @@ class TwigView extends View
 
     protected $paths = [];
 
+    /**
+     * Constructor
+     *
+     * @param \Cake\Network\Request    $request      Request instance.
+     * @param \Cake\Network\Response   $response     Response instance.
+     * @param \Cake\Event\EventManager $eventManager Event manager instance.
+     * @param array                    $viewOptions  View options. See View::$_passedVars for list of
+     *                                               options which get set as class properties.
+     */
     public function __construct(
         Request $request = null,
         Response $response = null,
@@ -65,6 +76,16 @@ class TwigView extends View
     }
 
     /**
+     * Get Twig
+     *
+     * @return Twig_Environment
+     */
+    public function getTwig()
+    {
+        return $this->twig;
+    }
+
+    /**
      * Renders view for given view file and layout.
      *
      * Render triggers helper callbacks, which are fired before and after the view are rendered,
@@ -79,7 +100,7 @@ class TwigView extends View
      * a plugin view/layout can be used instead of the app ones. If the chosen plugin is not found
      * the view will be located along the regular view path cascade.
      *
-     * @param string $view Name of view file to use
+     * @param string $view   Name of view file to use
      * @param string $layout Layout to use.
      *
      * @return string|null Rendered content or null if content already rendered and returned earlier.
@@ -92,11 +113,11 @@ class TwigView extends View
         }
 
         $renderedTemplate = '';
-        if ($view !== false && $viewFileName = $this->getViewFilename()) {
+        if ($view !== false && $viewFileName = $this->getViewFileName($view)) {
             $this->_currentType = static::TYPE_VIEW;
-            $this->eventManager()->dispatch(new Event('View.beforeRender', $this, array($viewFileName)));
-            $renderedTemplate = $this->_render($this->getViewFilename());
-            $this->eventManager()->dispatch(new Event('View.afterRender', $this, array($viewFileName)));
+            $this->eventManager()->dispatch(new Event('View.beforeRender', $this, [$viewFileName]));
+            $renderedTemplate = $this->_render($viewFileName);
+            $this->eventManager()->dispatch(new Event('View.afterRender', $this, [$viewFileName]));
         }
 
         $this->hasRendered = true;
@@ -108,7 +129,7 @@ class TwigView extends View
      * array of data. Handles parent/extended views.
      *
      * @param string $viewFile Filename of the view
-     * @param array $data Data to include in rendered view. If empty the current View::$viewVars will be used.
+     * @param array  $data     Data to include in rendered view. If empty the current View::$viewVars will be used.
      *
      * @throws Exception
      * @return string Rendered output
@@ -138,8 +159,8 @@ class TwigView extends View
     /**
      * Sandbox method to evaluate a template / view script in.
      *
-     * @param string $viewFile Filename of the view
-     * @param array $dataForView Data to include in rendered view.
+     * @param string $viewFile    Filename of the view
+     * @param array  $dataForView Data to include in rendered view.
      *                            If empty the current View::$viewVars will be used.
      *
      * @return string Rendered output
@@ -160,9 +181,9 @@ class TwigView extends View
      * Renders an element and fires the before and afterRender callbacks for it
      * and writes to the cache if a cache is used
      *
-     * @param string $file Element file path
-     * @param array $data Data to render
-     * @param array $options Element options
+     * @param string $file    Element file path
+     * @param array  $data    Data to render
+     * @param array  $options Element options
      *
      * @return string
      */
@@ -204,50 +225,117 @@ class TwigView extends View
         return $output;
     }
 
-    protected function getViewFilename()
+    /**
+     * Returns filename of given action's template file (.ctp) as a string.
+     * CamelCased action names will be under_scored! This means that you can have
+     * LongActionNames that refer to long_action_names.ctp views.
+     *
+     * @param string $name Controller action to find template filename for
+     *
+     * @return string Template filename
+     * @throws \Cake\View\Exception\MissingViewException when a view file could not be found.
+     */
+    protected function getViewFileName($name = null)
     {
-        if ($this->plugin) {
-            $filename = '@' . Inflector::underscore($this->plugin)
-                . DS . $this->viewPath
-                . DS . $this->view . $this->_ext;
-        } else {
-            $filename = $this->viewPath . DS . $this->view . $this->_ext;
+        $subDir = null;
+
+        if ($this->subDir !== null) {
+            $subDir = $this->subDir . DS;
         }
 
-        return $filename;
+        if ($name === null) {
+            $name = $this->view;
+        }
+        $name = str_replace('/', DS, $name);
+        list($plugin, $name) = $this->pluginSplit($name);
+
+        if (strpos($name, DS) === false && $name[0] !== '.') {
+            $name = $this->viewPath . DS . $subDir . Inflector::underscore($name);
+        } elseif (strpos($name, DS) !== false) {
+            if ($name[0] === DS || $name[1] === ':') {
+                if (is_file($name)) {
+                    return $name;
+                }
+                $name = trim($name, DS);
+            } elseif (!$plugin || $this->viewPath !== $this->name) {
+                $name = $this->viewPath . DS . $subDir . $name;
+            } else {
+                $name = DS . $subDir . $name;
+            }
+        }
+
+        $paths = Hash::merge(
+            $this->twig->getLoader()->getPaths($this->getPluginNamespace()),
+            $this->twig->getLoader()->getPaths()
+        );
+
+        foreach ($paths as $path) {
+            if (file_exists($path . DS . $name . $this->_ext)) {
+                $filename = $name . $this->_ext;
+
+                if ($this->plugin) {
+                    $filename = $this->getPluginNamespace(true) . DS . $filename;
+                }
+
+                return $filename;
+            }
+        }
+
+        throw new MissingViewException(array('file' => $name . $this->_ext));
     }
 
+    /**
+     * Get twig filesystem loader
+     *
+     * @return Twig_Loader_Filesystem
+     * @throws \Twig_Error_Loader
+     */
     protected function getFilesystemLoader()
     {
-        $mainPath = APP . 'Template' . DS;
+        $mainPath = CAKE . 'Template';
+        $appPath = APP . 'Template';
         $this->paths[] = ['path' => $mainPath, 'namespace' => Twig_Loader_Filesystem::MAIN_NAMESPACE];
-        $filesystemLoader = new Twig_Loader_Filesystem($mainPath);
+        $this->paths[] = ['path' => $appPath, 'namespace' => Twig_Loader_Filesystem::MAIN_NAMESPACE];
+        $filesystemLoader = new Twig_Loader_Filesystem([$appPath, $mainPath]);
 
-        foreach (Plugin::loaded() as $plugin) {
-            $templatePath = Plugin::classPath($plugin) . 'Template' . DS;
+        if ($this->plugin) {
+            $pluginTemplatePath = Plugin::classPath($this->plugin) . 'Template';
+            $appPluginPath = $appPath . 'Plugin' . DS . $this->plugin;
 
-            if (is_dir($templatePath)) {
-                $this->paths[] = ['path' => $templatePath, 'namespace' => Inflector::underscore($plugin)];
+            if (is_dir($pluginTemplatePath)) {
+                $this->paths[] = ['path' => $pluginTemplatePath, 'namespace' => $this->getPluginNamespace()];
 
                 if ($this->theme) {
                     $filesystemLoader->addPath(
-                        Plugin::classPath($this->theme) . 'Template' . DS,
-                        Inflector::underscore($plugin)
+                        Plugin::classPath($this->theme) . 'Template',
+                        Inflector::underscore($this->plugin)
                     );
                 }
-                $filesystemLoader->addPath($templatePath, Inflector::underscore($plugin));
+                $filesystemLoader->addPath($pluginTemplatePath, $this->getPluginNamespace());
+            }
+
+            if (is_dir($appPluginPath)) {
+                $this->paths[] = ['path' => $appPath, 'namespace' => $this->getPluginNamespace()];
+
+                $filesystemLoader->addPath($appPluginPath, $this->getPluginNamespace());
             }
         }
 
         return $filesystemLoader;
     }
 
+    /**
+     * Add default extensions
+     */
     protected function addExtensions()
     {
         $this->twig->addExtension(new Twig_Extension_Basic());
         $this->twig->addExtension(new Twig_Extension_I18n());
     }
 
+    /**
+     * Add default globals
+     */
     protected function addGlobals()
     {
         $this->twig->addGlobal('View', $this);
@@ -256,12 +344,18 @@ class TwigView extends View
     }
 
     /**
-     * Get Twig
+     * Get plugin namespace
      *
-     * @return Twig_Environment
+     * @param bool $withSign At sign (@) symbol
+     *
+     * @return string
      */
-    public function getTwig()
+    protected function getPluginNamespace($withSign = false)
     {
-        return $this->twig;
+        if ($withSign) {
+            return '@' . Inflector::underscore($this->plugin);
+        } else {
+            return Inflector::underscore($this->plugin);
+        }
     }
 }
